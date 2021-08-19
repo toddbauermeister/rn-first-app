2c03021ce5790bb497f096889397927b7dd79b0c
Todd Bauermeister <todd@octoco.ltd>
2021-08-19 17:56:19 +0200

    add onPress functionality

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('Open up App.js to start working on your app!')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Change text"></Button>
+      <Text>{text}</Text>
+      <Button title="Change text" onPress={() => setText('Button was pressed!')}></Button>
     </View>
   );
 }
